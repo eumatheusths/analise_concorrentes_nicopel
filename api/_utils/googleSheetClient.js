777e0f55b83d47aedd642bdb2d1f8d2013cc7c6d
Matheus Alexandre <130194413+eumatheusths@users.noreply.github.com>
2025-10-31 12:04:02 -0300

    fix: Corrigir construtor do GoogleSpreadsheet para v3

--- api/_utils/googleSheetClient.js
+++ api/_utils/googleSheetClient.js
@@ -5,8 +5,6 @@ import { JWT } from 'google-auth-library';
 const SPREADSHEET_ID = '1bannpc2_PAWFbDMymn5wiff87_dVSBFzAV_ce6arR_E';
 
 // Configura a autenticação (LÊ AS VARIÁVEIS DO "COFRE" DA VERCEL)
-// O teu erro "You must initialize some kind of auth" acontece aqui,
-// porque process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL está vazio.
 const serviceAccountAuth = new JWT({
   email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
   key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Corrige quebras de linha
@@ -15,11 +13,14 @@ const serviceAccountAuth = new JWT({
   ],
 });
 
-const doc = new GoogleSpreadsheet(SPREADSHEET_ID, serviceAccountAuth);
+// AQUI ESTÁ A CORREÇÃO:
+// O construtor da v3.3.0 SÓ aceita o ID.
+const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
 
 // Função para obter a página (sheet)
 export async function getSheet() {
-  await doc.useServiceAccountAuth(serviceAccountAuth); // <-- Importante para a v3.3.0
+  // A autenticação é feita AQUI na v3.3.0, e não no construtor
+  await doc.useServiceAccountAuth(serviceAccountAuth); 
   await doc.loadInfo(); // Carrega propriedades da planilha
   return doc.sheetsByIndex[0]; // Pega a primeira página da planilha
 }
